d7f4c4336c04dea7e8812e0fb3b8857763a2b73e
senadin <senadin.botic@kodecta.com>
2020-02-10 11:33:48 +0100

    Improve behaviour

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -187,5 +187,25 @@ $(document).ready(function () {
         });
     }
 
-
+    // Custom Table
+    if ($(window).width() > 767) {
+        $(".custom-table").each(function () {
+            var $this = $(this);
+            var newrows = [];
+            $this.find("tr").each(function () {
+                var i = 0;
+                $(this).find("td,th").each(function () {
+                    i++;
+                    if (newrows[i] === undefined) {
+                        newrows[i] = $("<tr></tr>");
+                    }
+                    newrows[i].append($(this));
+                });
+            });
+            $this.find("tr").remove();
+            $.each(newrows, function () {
+                $this.append(this);
+            });
+        });
+    }
 });
